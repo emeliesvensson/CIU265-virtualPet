407397a7739bfc1e6184d1eb5bbf2f2b40d05f21
Emelie Svensson <sakuradoud@hotmail.com>
2015-09-17 13:27:20 +0200

    Added the kitten from assetstore together with an extra kitten in the scene

---
+++ virtualPet/Assets/testAnim.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testAnim : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Animation>()["Ithcing"].layer  = 1;
+		GetComponent<Animation>()["Ithcing"].wrapMode = WrapMode.Once;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey ("k")) {
+			GetComponent<Animation> ().Play ("Ithcing");
+		} 
+
+	}
+
+}
